f808f08a2295601a6d7f5ab160480d19ecd76164
tomhebcit <the18@my.bcit.ca>
2025-03-27 16:30:16 -0700

    fixed auth error logging

--- js/teacherMain.js
+++ js/teacherMain.js
@@ -8,23 +8,24 @@ class utils {
     if (!token || !role || role !== "teacher") {
       console.log('No token or role found, redirecting to index.html');
       // window.location.href = "index.html";
+      return;
     }
     fetch(tokenEndpoint, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ token, role })
     })
-      .then(response => response.json())
-      .then(data => {
-        if (!data.ok) { 
-          console.error('Error:', data.message);
-          // window.location.href = "index.html";
-        }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
+    .then(response => {
+      if (!response.ok) {
+        console.error('Error: Response not OK');
         // window.location.href = "index.html";
-      });
+        return; 
+      }
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      // window.location.href = "index.html";
+    });
   }
 
   static setteacherMainStrings() {
